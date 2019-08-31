53c4e69063da28052a5f12316158ea941a0489a7
LMJian <1150696681@qq.com>
2019-08-31 10:22:15 +0800

    2019_08_31牛客网刷题练习代码

---
+++ 0831/0831/main.cpp
@@ -0,0 +1,91 @@
+/*#include<iostream> 
+#include<vector>
+using namespace std;
+
+/*typedef void  (*FUN)();
+class A
+{
+public:
+	const int _a;
+	A()
+		:_a(0)
+	{
+	}
+	A(int a)
+		:_a(a)
+	{
+	}
+};
+(*FUN)(int a)
+{
+	return a;
+}
+int fun()
+{
+	return 1;
+}*/
+/*int main()
+{
+	int a[36 + 6] = { 0 };
+	/*char s[10], (*k)[10], *p;
+	p = s;
+	p = k;
+	p = s;
+	k = s;
+	/*union A
+	{
+		char a[13] ;
+		char b;
+		int c;
+	};
+	A test = { 1 };
+	cout << sizeof(test) << endl;
+	/*int(*)pt()=fun();
+	//pt = &fun;
+	/*vector<int> A(10);
+	int n;
+	int count = 0, i, j;
+	cin >> n;
+	A.reserve(n);
+	A[count++] = 2;
+	for (i = 3; i < n; i++)
+	{
+		if (i % 2 == 0)
+			break;
+		j = 3;
+		while (j <= i / 2 && i%j != 0)
+			j += 2;
+		if (j>i / 2)
+			A[count++] = i;
+	}
+	/*string s = "123465";
+	cout << s.length() << endl;
+	/*int arr[] = { 1 };
+	cout << sizeof(arr) << endl;
+	/*union
+	{
+		int a =12;
+		char b ;
+		short c ;
+	};
+	//a = 12;
+	cout << b << endl;
+	cout << c << endl;
+	//int *a() = fun;
+	//a = &fun();
+	/*double a = 1.6, b = 6.2;
+	a++;
+
+	cout << a%b  << endl;
+	//int &a;
+	/*const int a = 9;
+	//a = 9;
+	/*int a = 7;
+	const int *pta;
+	pta = &a;
+	*pta = 1;
+	pta = &a+1;
+
+	system("pause");
+	return 0;
+}*/
---
+++ 0831/0831/test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	const int a = 10;
+	int arr[a+6] = { 0 };
+
+	system("pause");
+	return 0;
+}
