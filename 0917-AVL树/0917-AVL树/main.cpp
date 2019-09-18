40382675ba6b24eebc20376ad5fc42bfea8bd936
LMJian <1150696681@qq.com>
2019-09-18 23:48:54 +0800

    2019-9-18

--- 0917-AVL树/0917-AVL树/main.cpp
+++ 0917-AVL树/0917-AVL树/main.cpp
@@ -108,16 +108,12 @@ public:
 					if (1 == pParent->_pRight->_bf)//右右，左单旋
 					{
 						Left(pParent);
-						pParent->_bf = pParent->_pRight->_bf = 0;
 						break;
 					}
 					else//-1 == pParent->_pRight->_bf 
 						//右左，先右单旋，再左单旋
 					{
-						pParent->_bf = -1;
-						pParent->_pRight->_bf = pParent->_pRight->_pLeft->_bf = 0;
-						Right(pParent->_pRight);
-						Left(pParent);
+						RightLeft(pParent);
 						break;
 					}
 				}
@@ -126,15 +122,11 @@ public:
 					if (-1 == pParent->_pLeft->_bf)//左左，右单旋
 					{
 						Right(pParent);
-						pParent->_bf = pParent->_pLeft->_bf = 0;
 						break;
 					}
 					else//左右，先左旋，再右旋
 					{
-						pParent->_bf = 1;
-						pParent->_pLeft->_bf = pParent->_pLeft->_pRight->_bf = 0;
-						Left(pParent->_pLeft);
-						Right(pParent);
+						LeftRight(pParent);
 						break;
 					}
 				}
@@ -144,201 +136,125 @@ public:
 	}
 	void Left(PNode root)//左单旋
 	{
-		PNode pParent = root;
-		PNode pCur = root->_pRight;
+		PNode pParent = root->_pRight;
+		PNode pCur = pParent->_pLeft;
+		root->_pRight = pCur;
+		if (pCur != nullptr)
+			pCur->_pParent = root;
+		root->_pParent = pParent;
+		pParent->_pLeft = root;
 
-		if ((pParent->_pParent != nullptr) && (pParent->_pParent->_pLeft == pParent))
-		{		
-			pParent->_pParent->_pLeft = pCur;
-			pCur->_pParent = pParent->_pParent;
-		}
-		else if ((pParent->_pParent != nullptr) && (pParent->_pParent->_pRight == pParent))
+		if (root->_pParent == nullptr)
 		{
-			pParent->_pParent->_pRight = pCur;
-			pCur->_pParent = pParent->_pParent;
+			pParent->_pParent = nullptr;
+			_pRoot = pParent;
 		}
-		pParent->_pRight = pCur->_pLeft;
-		if (pCur->_pLeft != nullptr)
-			pCur->_pLeft->_pParent = pParent;
-		pParent->_pParent = pCur;
-		pCur->_pLeft = pParent;
-		if (root == _pRoot)
+		else
 		{
-			_pRoot = pCur;
-			_pRoot->_pParent = nullptr;
+			if (root->_pParent->_pLeft == root)
+			{
+				root->_pParent->_pLeft = pParent;
+				pParent->_pParent = root->_pParent;
+			}
+			else
+			{
+				root->_pParent->_pRight = pParent;
+				pParent->_pParent = root->_pParent;
+			}
 		}
+		root->_bf = pParent->_bf = 0;
 	}
 	void Right(PNode root)//右单旋
 	{
-		PNode pParent = root;
-		PNode pCur = root->_pLeft;
-
-		if ((pParent->_pParent != nullptr) && (pParent->_pParent->_pLeft == pParent))
+		PNode pParent = root->_pLeft;
+		PNode pCur = pParent->_pRight;
+		root->_pLeft = pCur;
+		if (pCur != nullptr)
+			pCur->_pParent = root;
+		root->_pParent = pParent;
+		pParent->_pRight = root;
+		if (root->_pParent == nullptr)
 		{
-			pParent->_pParent->_pLeft = pCur;
-			pCur->_pParent = pParent->_pParent;
+			pParent->_pParent = nullptr;
+			_pRoot = pParent;
 		}
-		else if ((pParent->_pParent != nullptr) && (pParent->_pParent->_pRight == pParent))
+		else
 		{
-			pParent->_pParent->_pRight = pCur;
-			pCur->_pParent = pParent->_pParent;
+			if (root->_pParent->_pLeft == root)
+			{
+				root->_pParent->_pLeft = pParent;
+				pParent->_pParent = root->_pParent;
+			}
+			else
+			{
+				root->_pParent->_pRight = pParent;
+				pParent->_pParent = root->_pParent;
+			}
 		}
-		pParent->_pLeft = pCur->_pRight;
-		if (pCur->_pRight!=nullptr)
-			pCur->_pRight->_pParent = pParent;
-		pCur->_pRight = pParent;
-		pParent->_pParent = pCur;
-		if (root == _pRoot)
-		{
-			_pRoot = pCur;
-			_pRoot->_pParent = nullptr;
-		}
-		root
+		root->_bf = pParent->_bf = 0;
+	}
+	void show()
+	{
+		_show(_pRoot);
+	}
+	void _show(PNode pt)
+	{
+		if (pt->_pLeft != nullptr)
+			_show(pt->_pLeft);
+		if (pt->_pRight != nullptr)
+			_show(pt->_pRight);
+		printf("结点：%d 度：%d \n", pt->_data, pt->_bf);
+	}
+	void LeftRight(PNode pParent)//左右旋
+	{
+		PNode pSubL = pParent->_pLeft;
+		PNode pSubLR = pSubL->_pRight;
+		int bf = pSubLR->_bf;
+		Left(pParent->_pLeft);
+		Right(pParent);
+		if (1 == bf)
+			pSubL->_bf = -1;
+		else if (-1 == bf)
+			pParent->_bf = 1;
+	}
+	void RightLeft(PNode pParent)//右左旋
+	{
+		PNode pSubR = pParent->_pRight;
+		PNode pSubRL = pSubR->_pLeft;
+		int bf = pSubRL->_bf;
+		Right(pParent->_pRight);
+		Left(pParent);
+		if (1 == bf)
+			pSubR->_bf = 1;
+		else if (-1 == bf)
+			pParent->_bf = -1;
 	}
 
-	/*bool Erase(const T& data)
+	int _Height(PNode pRoot);
+	bool _IsBalanceTree(PNode pRoot)//验证是否是AVL树
 	{
-		if (nullptr == _pRoot)
+		if (nullptr == pRoot)
+			return true;
+		int leftHeight = _Height(pRoot->_pLeft);
+		int rightHeight = _Height(pRoot->_pRight);
+		int diff = rightHeight - leftHeight;
+
+		if (diff != pRoot->_bf || (diff > 1 || diff < -1))
 			return false;
-		PNode pCur = _pRoot;
-		PNode pParent = nullptr;
-		while (pCur)
-		{
-			if (data == pCur->_data)
-				break;
-			else if (data < pCur->_data)
-			{
-				pParent = pCur;
-				pCur = pCur->_pLeft;
-			}
-			else
-			{
-				pParent = pCur;
-				pCur = pCur->_pRight;
-			}
-		}
-	LOOP:
-		if (nullptr == pCur)//不存在
-			return false;
-		if (nullptr == pCur->_pLeft)//已找到该节点，其左子树为空
-		{
-			if (pParent->_pLeft == pCur)//父节点的左子树是该节点
-			{
-				pParent->_pLeft = pCur->_pRight;
-				free(pCur);
-			}
-			else                         //父节点的右子树是该节点
-			{
-				pParent->_pRight = pCur->_pRight;
-				free(pCur);
-			}
-		}
-		else if (nullptr == pCur->_pRight)//右子树为空
-		{
-			if (pParent->_pLeft == pCur)
-			{
-				pParent->_pLeft = pCur->_pLeft;
-				free(pCur);
-			}
-			else
-			{
-				pParent->_pRight = pCur->_pLeft;
-				free(pCur);
-			}
-		}
-		else//左右子树都不为空
-		{
-			PNode pnr = pCur->_pRight;//右子树中最小节点
-			pParent = pCur;
-			while (pnr->_pLeft)
-			{
-				pParent = pnr;
-				pnr = pnr->_pLeft;
-			}
-			pCur->_data = pnr->_data;
-			pCur = pnr;
-			goto LOOP;
-		}
-		return true;
-	}*/
+		return _IsBalanceTree(pRoot->_pLeft) && _IsBalanceTree(pRoot->_pRight);
+	}
 private:
 	PNode _pRoot;
 }; 
 
-/*void _RotateR(PNode pParent)
-{
-	PNode pSubL = pParent->_pLeft;
-	PNode pSubLR = pSubL->_pRight;
-
-	pParent->_pLeft = pSubLR;
-	if (pSubLR)
-		pSubLR->_pParent = pParent;
-
-	pSubL->_pRight = pParent;
-
-	PNode pPParent = pParent->_pParent;
-
-	pParent->_pParent = pSubL;
-
-	pSubL->_pParent = pPParent;
-
-	if (NULL == pPParent)
-	{
-		_pRoot = pSubL;
-		pSubL->_pParent = NULL;
-	}
-	else
-	{
-		if (pPParent->_pLeft == pParent)
-			pPParent->_pLeft = pSubL;
-		else
-			pPParent->_pRight = pSubL;
-	}
-	
-	pParent->_bf = pSubL->_bf = 0;
-}
-
-void _RotateLR(PNode pParent)
-{
-	PNode pSubL = pParent->_pLeft;
-	PNode pSubLR = pSubL->_pRight;
-
-	int bf = pSubLR->_bf;
-
-	_RotateL(pParent->_pLeft);
-
-	_RotateR(pParent);
-
-	if (1 == bf)
-		pSubL->_bf = -1;
-	else if (-1 == bf)
-		pParent->_bf = 1;
-}
-
-int _Height(PNode pRoot);
-bool _IsBalanceTree(PNode pRoot)
-{
-	if (nullptr == pRoot)
-		return true;
-	int leftHeight = _Height(pRoot->_pLeft);
-	int rightHeight = _Height(pRoot->_pRight);
-	int diff = rightHeight - leftHeight;
-
-	if (diff != pRoot->_bf || (diff > 1 || diff < -1))
-		return false;
-	return _IsBalanceTree(pRoot->_pLeft) && _IsBalanceTree(pRoot->_pRight);
-}*/
 AVLTree<int> tree;
 int main()
 {
-
-	int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	//int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
 	for (auto e : a)
-	{
 		tree.Insert(e);
-	}
-
-
+	tree.show();
 
 	system("pause");
 	return 0;
