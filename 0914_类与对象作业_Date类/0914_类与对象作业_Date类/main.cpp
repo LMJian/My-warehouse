2a712bb859f00654313c6d280f62e3880f9cb3dc
LMJian <1150696681@qq.com>
2019-09-18 21:02:32 +0800

    2019-09-18

--- 0914_类与对象作业_Date类/0914_类与对象作业_Date类/main.cpp
+++ 0914_类与对象作业_Date类/0914_类与对象作业_Date类/main.cpp
@@ -6,9 +6,34 @@ class Date
 public:    
 	Date(int year = 1900, int month = 1, int day = 1)
 	{
-		_year = year;
-		_month = month;
-		_day = day;
+		if (Isture(year, month, day))//如果是合法日期
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+		{
+			cout << "输入有误，已设置为默认值" << endl;
+			_year = 1900;
+			_month = 1;
+			_day = 1;
+		}
+	}
+	bool Isture(int year, int month, int day)
+	{
+		if (year < 0 || month <= 0 || month>12)
+			return false;
+		if (day <= 0 || day > GetDayOfMonth(year, month))
+			return false;
+		return true;
+	}
+	int GetDayOfMonth(int year, int month)
+	{
+		int days[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+			days[2]++;
+		return days[month];
 	}
 	Date(const Date& d)
 	{
@@ -33,33 +58,15 @@ public:
 	Date operator+(int days)const
 	{
 		Date da(*this);
-		da._day += days;
-		if (da._day > monthDays())
-		{
-			da._month++;
-			da._day -= monthDays();
-			if (da._month > 12)
-			{
-				da._month = 1;
-				da._year++;
-			}
-		}
+		for (int i = 0; i < days; i++)
+			da++;
 		return da;
 	}
 	Date operator-(int days)
 	{
 		Date da(*this);
-		da._day -= days;
-		if (da._day <= 0)
-		{
-			da._month--;
-			da._day += da.monthDays();
-			if (da._month == 0)
-			{
-				da._month = 12;
-				da._year--;
-			}
-		}
+		for (int i = 0; i < days; i++)
+			da--;
 		return da;
 	}
 	int operator-(const Date& d)
@@ -93,12 +100,13 @@ public:
 		if (_day > monthDays())
 		{
 			_month++;
-			_day = _day - monthDays();
 			if (_month > 12)
 			{
 				_month = 1;
 				_year++;
 			}
+			_day = _day - monthDays();
+			
 		}
 		return *this;
 	}
@@ -109,12 +117,12 @@ public:
 		if (_day > monthDays())
 		{
 			_month++;
-			_day = _day - monthDays();
 			if (_month > 12)
 			{
 				_month = 1;
 				_year++;
 			}
+			_day = _day - monthDays();
 		}
 		return da;
 	}
@@ -124,12 +132,12 @@ public:
 		if (_day <= 0)
 		{
 			_month--;
-			_day += monthDays();
 			if (_month == 0)
 			{
 				_month = 12;
 				_year--;
 			}
+			_day += monthDays();
 		}
 		return *this;
 	}
@@ -140,12 +148,12 @@ public:
 		if (_day <= 0)
 		{
 			_month--;
-			_day += monthDays();
 			if (_month == 0)
 			{
 				_month = 12;
 				_year--;
 			}
+			_day += monthDays();
 		}
 		return da;
 	}
@@ -196,9 +204,15 @@ private:
 };
 int main()
 {
+	Date d1(2015, 12, 31);
+	Date d2(2015, 1, 1);
 
-	Date d1(2019, 12, 31);
-	Date d2(2016, 1, 1);
+	Date d3 = d1 -365;
+	//cout << (d1 - d2) << endl;
+	//d1++;2
+	//d2--;
+	//Date d3 = --d1;
+	//Date d4 = --d2;
 	/*cout << (d1 > d2) << endl;
 	cout << (d1 >= d2) << endl;
 	cout << (d1 < d2) << endl;
