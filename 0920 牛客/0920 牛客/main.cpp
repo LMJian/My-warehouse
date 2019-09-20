af4e95842fffac8b022215ef7b4906f91da9e7fb
LMJian <1150696681@qq.com>
2019-09-20 23:57:32 +0800

    2019-9-20

---
+++ 0920 牛客/0920 牛客/main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	/*int i = 100;
+	cout.unsetf(ios::dec);
+	cout.setf(ios::hex);
+	cout << i << "\t";
+	cout << i << "\t";
+	cout.setf(ios::dec);
+	cout << i << "\t";
+	/*char p1[10] = "abcd", *p2, str[10] = "xyz";
+	p2 = "ABCD";
+	cout << strcat(p1 + 2, p2 + 1) << endl;
+	//strcpy(str + 2, );
+
+	printf("%s\n", str);*/
+	system("pause");
+	return 0;
+}
