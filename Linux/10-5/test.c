6f7dc800367cc2efc117c0ff6da53b2e852aed09
LMJian <1150696681@qq.com>
2019-10-05 16:16:12 +0800

    2019-10-5-文件函数学习

---
+++ Linux/10-5/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+  char *file="./tmp.txt";
+  FILE *fp=fopen(file,"w+");
+  if(fp==NULL)
+  {
+    perror("fopen error");
+    return -1;
+  }
+  char buf[1024]="下课吃饭~~\n";
+  fwrite(buf,1,strlen(buf),fp);
+  fseek(fp,0,SEEK_SET);
+  memset(buf,0x00,1024);
+  int ret=fread(buf,1,1023,fp);
+  //perror("fread error");
+  if(feof(fp))
+  {
+    printf("end of file!!\n");
+  }
+  printf("buf:[%d-%s]\n",ret,buf);
+  fclose(fp);
+  return 0;
+}
