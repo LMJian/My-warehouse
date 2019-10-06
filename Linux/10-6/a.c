9966178ed1847b7edcd4d4a488eb5cce2594ba99
LMJian <1150696681@qq.com>
2019-10-06 22:15:41 +0800

    2019-10-6

---
+++ Linux/10-6/a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<errno.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include"b.h"
+
+int main(int argc,char *argv[])
+{
+  print();
+  printf("hello bit!!\n");
+  return 0;
+}
---
+++ Linux/10-6/b.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<errno.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int print()
+{
+  printf("hello print\n");
+  return 0;
+}
