6767b287d323ef400fd6e6e916beb58e8fcfdf82
LMJian <1150696681@qq.com>
2019-10-24 09:35:03 +0800

    2019-10-24-更新

--- C++/10-23-优先队列/10-23-优先队列/main.cpp
+++ C++/10-23-优先队列/10-23-优先队列/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<functional>
+//#include<functional>
 using namespace std;
 
 class Date
@@ -34,18 +34,101 @@ private:
 	int _month;
 	int _day;
 };
-
-class Less
+template<class T>
+class Sess
 {
 public:
-	bool operator()(const Date* pLeft, const Date* pRight)
+	bool operator()(const T pLeft, const T pRight)
 	{
 		return *pLeft < *pRight;
 	}
 };
+
+
+/*template<class T, class Sequence = vector<T>, class Compare = less<T>>
+class priority_queue
+{
+public:
+	priority_queue()
+		:_seq()
+	{}
+	template<class InputIt>
+	priority_queue(InputIt start, InputIt last)
+		: _seq(start, last)
+	{
+		int parent = (_seq.size() - 2) >> 1;
+		while (parent >= 0)
+		{
+			makeDown(parent);
+			parent--;
+		}
+	}
+	void push(const T& x)
+	{
+		_seq.push_back(x);
+		makeUp(_seq.size() - 1);
+	}
+	void pop()
+	{
+		if (empty())
+			return;
+		//swap(_seq.begin(), _seq.end() - 1);
+		swap(_seq.front(), _seq.back());
+		_seq.pop_back();
+		makeDown(0);
+	}
+	bool empty()const
+	{
+		return _seq.empty();
+	}
+	const T& top()const
+	{
+		return _seq.front();
+	}
+	size_t size()const
+	{
+		return _seq.size();
+	}
+private:
+	void makeUp(int child)
+	{
+		int parent = (child - 1) >> 1;
+		while (child)
+		{
+			if (_com(_seq[parent], _seq[child]))
+			{
+				swap(_seq[parent], _seq[child]);
+				child = parent;
+				parent = (child - 1) >> 1;
+			}
+			else
+				return;
+		}
+	}
+	void makeDown(int parent)
+	{
+		int child = parent * 2 + 1;
+		while (child < _seq.size())
+		{
+			if (child + 1 < _seq.size() && _com(_seq[child], _seq[child + 1]))
+				++child;
+			if (_com(_seq[parent], _seq[child]))
+			{
+				swap(_seq[parent], _seq[child]);
+				parent = child;
+				child = parent * 2 + 1;
+			}
+			else
+				break;
+		}
+	}
+	Sequence _seq;
+	Compare _com;
+};*/
+
 void TestPriorityQueue()
 {
-	priority_queue<Date*, vector<Date*>, Less>q;
+	priority_queue<Date*,vector<Date*>,Sess<Date*>> q;
 	q.push(&Date(2019, 10, 23));
 	q.push(&Date(2019, 10, 24));
 	q.push(&Date(2019, 10, 25));
@@ -63,9 +146,9 @@ void TestPriorityQueue()
 	q2.push(Date(2019, 10, 24));
 	q2.push(Date(2019, 10, 25));
 
-	cout << q2.top() << endl;
+	cout << q2.top() << endl;*/
 	/*vector<int> v{ 3,2,7,6,0,4,1,9,8,5 };
-	priority_queue<int> q1;
+	/*priority_queue<int> q1;
 	for (auto& e : v)
 		q1.push(e);
 	cout << q1.top() << endl;
