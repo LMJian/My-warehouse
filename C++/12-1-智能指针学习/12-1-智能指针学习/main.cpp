912e245842e7dcd20da24d696962c9d15b8083fa
LMJian <1150696681@qq.com>
2019-12-01 21:57:14 +0800

    2019-12-1-实现线程安全的shared_ptr-实现了删除器-利用weak_ptr解决循环引用问题

--- C++/12-1-智能指针学习/12-1-智能指针学习/main.cpp
+++ C++/12-1-智能指针学习/12-1-智能指针学习/main.cpp
@@ -3,16 +3,17 @@
 #include<thread>
 using namespace std;
 
-template<class T>
-class DFDef {
-public:
-	void operator()(T*& _ptr) {
-		delete _ptr;
-		_ptr = nullptr;
-	}
-};
-
 namespace LMJian {
+
+	template<class T>
+	class DFDef {
+	public:
+		void operator()(T*& _ptr) {
+			delete _ptr;
+			_ptr = nullptr;
+		}
+	};
+
 	template<class T, class DF = DFDef<T>>
 	class shared_ptr {
 	public:
@@ -88,106 +89,110 @@ namespace LMJian {
 		int* _ret;
 		mutex* _mutex;
 	};
-}
 
-struct Date
-{
-	Date()
+	struct Date
 	{
-		_year = _month = _day = 0;
-	}
+		Date()
+		{
+			_year = _month = _day = 0;
+		}
 
-	int _year;
-	int _month;
-	int _day;
-};
+		int _year;
+		int _month;
+		int _day;
+	};
 
-class Test {
-public:
-	Test() {
-		_int = new int(1);
-	}
-	~Test() {
-		free(_int);
-	}
-private:
-	int* _int;
-};
+	class Test {
+	public:
+		Test() {
+			_int = new int(1);
+		}
+		~Test() {
+			free(_int);
+		}
+	private:
+		int* _int;
+	};
 
 #if 0
-/*void test_shared_ptr()
-{
-	//Test arr[10];
-	LMJian::shared_ptr<Test> sp1(new Test[10]);
+	/*void test_shared_ptr()
+	{
+		//Test arr[10];
+		LMJian::shared_ptr<Test> sp1(new Test[10]);
 
-	/*LMJian::shared_ptr<int> sp1(new int);
-	cout << sp1.use_count() << endl;
+		/*LMJian::shared_ptr<int> sp1(new int);
+		cout << sp1.use_count() << endl;
 
-	LMJian::shared_ptr<int> sp2(sp1);
-	cout << sp1.use_count() << endl;
-	cout << sp2.use_count() << endl;
+		LMJian::shared_ptr<int> sp2(sp1);
+		cout << sp1.use_count() << endl;
+		cout << sp2.use_count() << endl;
 
 
-	LMJian::shared_ptr<int> sp3(new int);
-	cout << sp3.use_count() << endl;
+		LMJian::shared_ptr<int> sp3(new int);
+		cout << sp3.use_count() << endl;
 
-	LMJian::shared_ptr<int> sp4(sp3);
-	cout << sp3.use_count() << endl;
-	cout << sp4.use_count() << endl;
+		LMJian::shared_ptr<int> sp4(sp3);
+		cout << sp3.use_count() << endl;
+		cout << sp4.use_count() << endl;
 
-	sp3 = sp2;
-	cout << sp2.use_count() << endl;
-	cout << sp3.use_count() << endl;
+		sp3 = sp2;
+		cout << sp2.use_count() << endl;
+		cout << sp3.use_count() << endl;
 
-	sp4 = sp2;
-	cout << sp2.use_count() << endl;
-	cout << sp4.use_count() << endl;
-}*/
+		sp4 = sp2;
+		cout << sp2.use_count() << endl;
+		cout << sp4.use_count() << endl;
+	}*/
 #endif
 
-
-
-void SharePtrFunc(LMJian::shared_ptr<Date>& sp, size_t n)
-{
-	for (size_t i = 0; i < n; ++i)
+	void SharePtrFunc(shared_ptr<Date>& sp, size_t n)
 	{
-		LMJian::shared_ptr<Date> copy(sp);
+		for (size_t i = 0; i < n; ++i)
+		{
+			shared_ptr<Date> copy(sp);
 
-		copy->_year++;
-		copy->_month++;
-		copy->_day++;
+			copy->_year++;
+			copy->_month++;
+			copy->_day++;
+		}
 	}
 }
-struct ListNode {
-	ListNode(int ret)
-		:_ret(ret)
+	struct ListNode {
+		ListNode(int ret)
+			:_ret(ret)
+		{
+			cout << "ListNode" << endl;
+		}
+		~ListNode() {
+			cout << "~ListNode" << endl;
+		}
+		//LMJian::shared_ptr<ListNode> _pre;
+		//LMJian::shared_ptr<ListNode> _next;
+		weak_ptr<ListNode> _pre;
+		weak_ptr<ListNode> _next;
+		int _ret;
+	};
+
+	void testlist()
 	{
-		cout << "ListNode" << endl;
+		shared_ptr<ListNode> sp1(new ListNode(10));
+		shared_ptr<ListNode> sp2(new ListNode(20));
+
+		cout << sp1.use_count() << endl;
+		cout << sp2.use_count() << endl;
+
+		sp1->_next = sp2;
+		sp2->_pre = sp1;
+
+		cout << sp1.use_count() << endl;
+		cout << sp2.use_count() << endl;
 	}
-	~ListNode() {
-		cout << "~ListNode" << endl;
-	}
-	LMJian::shared_ptr<ListNode> _pre;
-	LMJian::shared_ptr<ListNode> _next;
-	int _ret;
-};
 
-void testlist()
-{
-	LMJian::shared_ptr<ListNode> sp1(new ListNode(10));
-	LMJian::shared_ptr<ListNode> sp2(new ListNode(20));
-
-	cout << sp1.use_count() << endl;
-	cout << sp2.use_count() << endl;
-
-	sp1->_next = sp2;
-	sp2->_pre = sp1;
-
-	cout << sp1.use_count() << endl;
-	cout << sp2.use_count() << endl;
-}	
 int main() {
 	testlist();
+	//weak_ptr<int> p1(nullptr);
+	//weak_ptr<int> p2(new int);
+	weak_ptr<int> p3;
 	/*shared_ptr<Date> sp(new Date);
 
 	thread t1(SharePtrFunc, sp, 10000);
