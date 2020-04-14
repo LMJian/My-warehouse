7036422ccdae791775dbaf21a69c96016bb62d2d
LMJian <1150696681@qq.com>
2020-04-14 16:46:57 +0800

    2020-4-14-每日一题

---
+++ C++/4-14-每日一题/4-14-每日一题/main.cpp
@@ -0,0 +1,33 @@
+// write your code here cpp
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int Count(int n) {
+	if (n <= 3)
+		return n;
+	int a = 2;
+	int b = 3;
+	int c;
+	for (int i = 4; i <= n; ++i) {
+		c = (a + b) % 10000;
+		a = b;
+		b = c;
+	}
+	return c;
+}
+int main() {
+	int n;
+	vector<int> arr;
+	while (cin >> n) {
+		arr.resize(n);
+		for (int i = 0; i < n; ++i)
+			cin >> arr[i];
+		for (int i = 0; i < n; ++i) {
+			printf("%04d", Count(arr[i]));
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
