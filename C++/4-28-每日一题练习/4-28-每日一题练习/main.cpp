4b3380d7fe2eb2a48371aa643d719dabf05f16f3
LMJian <1150696681@qq.com>
2020-04-29 00:13:57 +0800

    2020-4-29-每日一题练习

--- C++/4-28-每日一题练习/4-28-每日一题练习/main.cpp
+++ C++/4-28-每日一题练习/4-28-每日一题练习/main.cpp
@@ -1,4 +1,4 @@
-// write your code here cpp
+/*// write your code here cpp
 #include<iostream>
 #include<vector>
 #include<string>
@@ -21,6 +21,44 @@ int main() {
 				cout << "mkdir -p " << arr[i] << endl;
 		}
 		cout << "mkdir -p " << arr[n - 1] << endl;
+	}
+	return 0;
+}*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n, m, k;
+int x, y;
+vector<vector<char>> arr;
+
+void dfs(int x, int y, bool key, int& a, int& b) {
+	if (x == n && y == m) {
+		++b;
+		if (key)
+			++a;
+		return;
+	}
+	if (arr[x][y] == -1)
+		key = true;
+	if (x + 1 <= n)
+		dfs(x + 1, y, key, a, b);
+	if (y + 1 <= m)
+		dfs(x, y + 1, key, a, b);
+}
+int main() {
+	while (cin >> n >> m >> k) {
+		arr.clear();
+		arr.resize(n + 1, vector<char>(m + 1, 0));
+		for (int i = 0; i < k; ++i) {
+			cin >> x >> y;
+			arr[x][y] = -1;
+		}
+		int a = 0;
+		int b = 0;
+		dfs(1, 1, false, a, b);
+		printf("%.2f\n", 1.0*a / b);
 	}
 	return 0;
 }
