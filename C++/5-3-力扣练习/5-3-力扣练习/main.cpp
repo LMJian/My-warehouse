ffacedf317a2884ad385deb584a74ccc43e77463
LMJian <1150696681@qq.com>
2020-05-03 23:56:11 +0800

    2020-5-3-力扣练习

--- C++/5-3-力扣练习/5-3-力扣练习/main.cpp
+++ C++/5-3-力扣练习/5-3-力扣练习/main.cpp
@@ -3,7 +3,7 @@
 #include<stack>
 using namespace std;
 
-class Solution {
+/*class Solution {
 public:
 	bool isValid(string s) {
 		stack<char> sa;
@@ -27,11 +27,40 @@ private:
 			return true;
 		return false;
 	}
+};*/
+#include<vector>
+#include<algorithm>
+class Solution {
+public:
+	vector<string> generateParenthesis(int n) {
+		vector<string> ret;
+		string str(n, '(');
+		str += string(n, ')');
+		do {
+			if (isValid(str))
+				ret.push_back(str);
+		} while (next_permutation(str.begin(), str.end()));
+		return ret;
+	}
+private:
+	bool isValid(const string& s) {
+		stack<char> sa;
+		const char* ptr = s.c_str();
+		while (*ptr != '\0') {
+			if (sa.empty() || (!(sa.top() == '('&&*ptr == ')')))
+				sa.push(*ptr);
+			else
+				sa.pop();
+			++ptr;
+		}
+		return sa.empty();
+	}
 };
-
 int main() {
 	Solution sa;
-	cout<<sa.isValid("()")<<endl;
+	sa.generateParenthesis(3);
+	//Solution sa;
+	//cout<<sa.isValid("()")<<endl;
 	system("pause");
 	return 0;
 }
